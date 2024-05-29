2c23afa3643739be36d387ce10d5c76277d7cc08
paaton <paatzondrej@seznam.cz>
2024-05-29 02:59:38 +0200

    added action for troop entry

--- public/sendEntryRequest.js
+++ public/sendEntryRequest.js
@@ -28,3 +28,34 @@ function sendEntryRequest(form)
         }
     });
 }
+
+
+function sendTroopEntryRequest(form) {
+    form.addEventListener('submit', async event => {
+        event.preventDefault();
+        const resultElement = form.getElementsByClassName("formResult")[0];
+        resultElement.innerText = "↻";
+
+        try {
+            const data = new FormData(form);
+            const response = await fetch(
+                form.action,
+                {
+                    method: 'POST',
+                    body: data,
+                    headers: {
+                        'authorization': 'Bearer ' + document.cookie.match(new RegExp('(^| )bearerToken=([^;]+)'))[2],
+                    }
+                },
+            );
+
+            const responseData = await response.json();
+            if (response.ok) {
+                resultElement.innerText = "✔"
+            }
+        } catch (error) {
+            resultElement.innerText = "☠";
+            console.log(error.message);
+        }
+    });
+}
